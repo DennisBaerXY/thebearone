8b506e11bc5e3dfbc007823d6c4020750fcd5958
Dennis Bär <dennis.baer@student.reutlingen-university.de>
2021-05-05 11:38:45 +0200

    Changes

--- server/db.js
+++ server/db.js
@@ -1,17 +1,22 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-const devConfig = {
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD.toString(),
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-};
+let proConfig;
+let devConfig;
+if (process.env.NODE_ENV !== "production") {
+  devConfig = {
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD.toString(),
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+  };
+} else {
+  proConfig = {
+    connectionString: process.env.DATABASE_URL,
+  };
+}
 
-const proConfig = {
-  connectionString: process.env.DATABASE_URL,
-};
 const pool = new Pool(
   process.env.NODE_ENV === "production" ? proConfig : devConfig
 );
