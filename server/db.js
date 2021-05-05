1b74e7500d4c83a373f35078287a377badaef078
Dennis Bär <dennis.baer@student.reutlingen-university.de>
2021-05-05 11:40:48 +0200

    Server fix

--- server/db.js
+++ server/db.js
@@ -1,22 +1,19 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-let proConfig;
-let devConfig;
-if (process.env.NODE_ENV !== "production") {
-  devConfig = {
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD.toString(),
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE,
-  };
-} else {
-  proConfig = {
-    connectionString: process.env.DATABASE_URL,
-  };
-}
+const devConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD
+    ? process.env.PG_PASSWORD.toString()
+    : process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+};
 
+const proConfig = {
+  connectionString: process.env.DATABASE_URL,
+};
 const pool = new Pool(
   process.env.NODE_ENV === "production" ? proConfig : devConfig
 );
