c309721ecddaf5d745695950c4622470bc17c88b
Dennis Bär <dennis.baer@student.reutlingen-university.de>
2021-05-05 11:24:37 +0200

    PG Fixed

--- server/db.js
+++ server/db.js
@@ -9,8 +9,6 @@ const devConfig = {
   database: process.env.PG_DATABASE,
 };
 
-//const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-
 const proConfig = {
   connectionString: process.env.DATABASE_URL,
 };
