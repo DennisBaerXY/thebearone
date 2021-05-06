074d63ff7394240f494ef5e049c8d9dd51cc90bd
Dennis Bär <dennis.baer@student.reutlingen-university.de>
2021-05-06 12:06:24 +0200

    added ssl pg

--- server/db.js
+++ server/db.js
@@ -13,6 +13,9 @@ const devConfig = {
 
 const proConfig = {
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 const pool = new Pool(
   process.env.NODE_ENV === "production" ? proConfig : devConfig
